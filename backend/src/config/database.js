e360a42851cef76036adc4632661168d4956b58e
Dev-Onacc <poum.paul@togettech.ca>
2025-07-06 05:51:27 +0100

    Upate database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,23 +1,13 @@
 // backend/src/config/database.js
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-
-dotenv.config();  // charge les variables depuis .env
+dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,    // nom de la BDD
-  process.env.DB_USER,    // utilisateur
-  process.env.DB_PASS,    // mot de passe
+  process.env.DATABASE_URL || 
+  `${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
-    dialect: 'mysql',
-    logging: false,        // passez à console.log pour debug
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false,
   }
 );
